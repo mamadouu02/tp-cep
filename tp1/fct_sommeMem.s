ea2eceddf636aea91d1c4bda8551d5e092e65ddd
Mamadou <mamadou.thiongane@grenoble-inp.org>
2023-03-17 18:54:29 +0100

    sommeMem

--- tp1/fct_sommeMem.s
+++ tp1/fct_sommeMem.s
@@ -15,26 +15,32 @@ uint32_t sommeMem(void)
     .globl sommeMem
 /* DEBUT DU CONTEXTE
 Fonction :
-    nom_de_fonction : feuille ou non feuille
+    sommeMem : feuille
 Contexte :
-    parametre_0        : registre a0
-    parametre_1        : registre ai; pile *(sp+n)        # 2 possibilités
-    parametre_64_bits  : registres t0 / t1                # 2 registres necessaires
-    variable_locale0   : registre t0
-    variable_locale1   : pile *(sp+k)
-    ra                 : pile *(sp+p)                     # cas particulier d'un registre à sauver
-    variable_64_bits   : pile *(sp+k1) / *(sp+k2)         # 2 registres necessaires
-    variable_globale0  : memoire
-    variable_globale1  : memoire, section nom_de_section  # ex de section : .data, .bss, .text, .rodata
+    res : memoire
+    i : registre t0
 FIN DU CONTEXTE */
 sommeMem:
 sommeMem_fin_prologue:
 /* A compléter */
+    sw zero, res, t2
+    li t0, 1
+loop:
+    slti t1, t0, 11
+    beqz t1, endloop
+    lw t1, res
+    add t1, t1, t0
+    sw t1, res, t2
+    addi t0, t0, 1
+    j loop
+endloop:
+    lw a0, res
 sommeMem_debut_epilogue:
     ret
 
     .data
     .globl res
+    .comm res, 4
 /* uint32_t res;
   La variable globale res étant définie dans ce fichier, il est nécessaire de
   la définir dans la section .data du programme assembleur.

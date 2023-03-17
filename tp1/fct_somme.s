0f2798efa09e630883fb22834d9d1c8e175a2337
Mamadou <mamadou.thiongane@grenoble-inp.org>
2023-03-17 16:16:45 +0100

    somme

--- tp1/fct_somme.s
+++ tp1/fct_somme.s
@@ -13,20 +13,23 @@ uint32_t somme(void)
     .globl somme
 /* DEBUT DU CONTEXTE
 Fonction :
-    nom_de_fonction : feuille ou non feuille
+    somme : feuille
 Contexte :
-    parametre_0        : registre a0
-    parametre_1        : registre ai; pile *(sp+n)        # 2 possibilités
-    parametre_64_bits  : registres t0 / t1                # 2 registres necessaires
-    variable_locale0   : registre t0
-    variable_locale1   : pile *(sp+k)
-    ra                 : pile *(sp+p)                     # cas particulier d'un registre à sauver
-    variable_64_bits   : pile *(sp+k1) / *(sp+k2)         # 2 registres necessaires
-    variable_globale0  : memoire
-    variable_globale1  : memoire, section nom_de_section  # ex de section : .data, .bss, .text, .rodata
+    res : registre t0
+    i : registre t1
 FIN DU CONTEXTE */
 somme:
 somme_fin_prologue:
 /* A compléter */
+    li t0, 0
+    li t1, 1
+    li t2, 11
+loop:
+    beq t1, t2, endloop
+    add t0, t0, t1
+    addi t1, t1, 1
+    j loop
+endloop:
+    mv a0, t0
 somme_debut_epilogue:
     ret

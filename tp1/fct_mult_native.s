aa246beb8e7f6cf1cf64cf9265a2d57f9fb8cd32
Mamadou <mamadou.thiongane@grenoble-inp.org>
2023-03-19 02:26:17 +0100

    mult_native

--- tp1/fct_mult_native.s
+++ tp1/fct_mult_native.s
@@ -10,20 +10,16 @@ uint32_t mult_native(void)
     .globl mult_native
 /* DEBUT DU CONTEXTE
 Fonction :
-    nom_de_fonction : feuille ou non feuille
+    mult_native : feuille
 Contexte :
-    parametre_0        : registre a0
-    parametre_1        : registre ai; pile *(sp+n)        # 2 possibilités
-    parametre_64_bits  : registres t0 / t1                # 2 registres necessaires
-    variable_locale0   : registre t0
-    variable_locale1   : pile *(sp+k)
-    ra                 : pile *(sp+p)                     # cas particulier d'un registre à sauver
-    variable_64_bits   : pile *(sp+k1) / *(sp+k2)         # 2 registres necessaires
-    variable_globale0  : memoire
-    variable_globale1  : memoire, section nom_de_section  # ex de section : .data, .bss, .text, .rodata
+    x   : memoire
+    y   : memoire
 FIN DU CONTEXTE */
 mult_native:
 mult_native_fin_prologue:
 /* A compléter */
+    lw t0, x
+    lw t1, y
+    mul a0, t0, t1
 mult_native_debut_epilogue:
     ret

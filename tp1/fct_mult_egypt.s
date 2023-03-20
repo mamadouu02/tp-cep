4713d2d5343680759c1a0c60b0de1e15769be721
Mamadou <mamadou.thiongane@grenoble-inp.org>
2023-03-20 01:08:32 +0100

    fct_mult_egypt

--- tp1/fct_mult_egypt.s
+++ tp1/fct_mult_egypt.s
@@ -19,20 +19,31 @@ uint32_t mult_egypt(void)
 /* Attention, res est une variable locale que l'on mettra dans t0 */
 /* DEBUT DU CONTEXTE
 Fonction :
-    nom_de_fonction : feuille ou non feuille
+    mult_egypt : feuille
 Contexte :
-    parametre_0        : registre a0
-    parametre_1        : registre ai; pile *(sp+n)        # 2 possibilités
-    parametre_64_bits  : registres t0 / t1                # 2 registres necessaires
-    variable_locale0   : registre t0
-    variable_locale1   : pile *(sp+k)
-    ra                 : pile *(sp+p)                     # cas particulier d'un registre à sauver
-    variable_64_bits   : pile *(sp+k1) / *(sp+k2)         # 2 registres necessaires
-    variable_globale0  : memoire
-    variable_globale1  : memoire, section nom_de_section  # ex de section : .data, .bss, .text, .rodata
+    x  : memoire
+    y  : memoire
+    res  : registre t0
 FIN DU CONTEXTE */
 mult_egypt:
 mult_egypt_fin_prologue:
 /* A compléter */
+    li t0, 0
+while:
+    lw t2, y
+    beqz t2, endwhile
+if:
+    lw t1, x
+    andi t3, t2, 1
+    beqz t3, endif
+    add t0, t0, t1
+endif:
+    slli t1, t1, 1
+    sw t1, x, t3
+    srli t2, t2, 1
+    sw t2, y, t3
+    j while
+endwhile:
+    mv a0, t0
 mult_egypt_debut_epilogue:
     ret

58a614c31c0001b9e1316d041e422cdd07ba5a7e
Mamadou <mamadou.thiongane@grenoble-inp.org>
2023-03-24 16:57:03 +0100

    affine

--- tp2/fct_affine.s
+++ tp2/fct_affine.s
@@ -12,23 +12,23 @@ uint32_t affine(uint32_t a, uint32_t b, uint32_t x)
 Fonction :
     affine : non feuille
 Contexte :
-    a  : registre a0
-    b  : registre a1; pile *(sp+0)
-    x  : registre a2
-    ra  : pile *(sp+4)
+    a  : registre a0; pile *(sp+0)
+    b  : registre a1; pile *(sp+4)
+    x  : registre a2; pile *(sp+8)
+    ra  : pile *(sp+16)
 FIN DU CONTEXTE */
 affine:
 /* A compléter */
-    addi sp, sp, -2*4
-    sw ra, 1*4(sp)
-    sw a1, 0*4(sp)
+    addi sp, sp, -4*4
+    sw ra, 3*4(sp)
+    sw a1, 1*4(sp)
 affine_fin_prologue:
     mv a1, a0
     mv a0, a2
     jal mult
-    lw a1, 0*4(sp)
+    lw a1, 1*4(sp)
     add a0, a0, a1
 affine_debut_epilogue:
-    lw ra, 1*4(sp)
-    addi sp, sp, 2*4
+    lw ra, 3*4(sp)
+    addi sp, sp, 4*4
     ret

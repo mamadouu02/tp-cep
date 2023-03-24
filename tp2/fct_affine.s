f511fda51f1b56cb4a5d14bd7100672bb275b215
Mamadou <mamadou.thiongane@grenoble-inp.org>
2023-03-24 16:51:15 +0100

    affine

--- tp2/fct_affine.s
+++ tp2/fct_affine.s
@@ -10,20 +10,25 @@ uint32_t affine(uint32_t a, uint32_t b, uint32_t x)
     /* uint32_t affine(uint32_t a, uint32_t b, uint32_t x) */
 /* DEBUT DU CONTEXTE
 Fonction :
-    nom_de_fonction : feuille ou non feuille
+    affine : non feuille
 Contexte :
-    parametre_0        : registre a0
-    parametre_1        : registre ai; pile *(sp+n)        # 2 possibilités
-    parametre_64_bits  : registres t0 / t1                # 2 registres necessaires
-    variable_locale0   : registre t0
-    variable_locale1   : pile *(sp+k)
-    ra                 : pile *(sp+p)                     # cas particulier d'un registre à sauver
-    variable_64_bits   : pile *(sp+k1) / *(sp+k2)         # 2 registres necessaires
-    variable_globale0  : memoire
-    variable_globale1  : memoire, section nom_de_section  # ex de section : .data, .bss, .text, .rodata
+    a  : registre a0
+    b  : registre a1; pile *(sp+0)
+    x  : registre a2
+    ra  : pile *(sp+4)
 FIN DU CONTEXTE */
 affine:
 /* A compléter */
+    addi sp, sp, -2*4
+    sw ra, 1*4(sp)
+    sw a1, 0*4(sp)
 affine_fin_prologue:
+    mv a1, a0
+    mv a0, a2
+    jal mult
+    lw a1, 0*4(sp)
+    add a0, a0, a1
 affine_debut_epilogue:
+    lw ra, 1*4(sp)
+    addi sp, sp, 2*4
     ret

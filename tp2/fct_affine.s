a1608e015f538c2f6ba9cbfbea30c75959e021fc
Mamadou <mamadou.thiongane@grenoble-inp.org>
2023-03-25 23:46:34 +0100

    affine

--- tp2/fct_affine.s
+++ tp2/fct_affine.s
@@ -15,20 +15,20 @@ Contexte :
     a  : registre a0; pile *(sp+0)
     b  : registre a1; pile *(sp+4)
     x  : registre a2; pile *(sp+8)
-    ra  : pile *(sp+16)
+    ra  : pile *(sp+12)
 FIN DU CONTEXTE */
 affine:
 /* A compléter */
-    addi sp, sp, -4*4
-    sw ra, 3*4(sp)
-    sw a1, 1*4(sp)
+    addi sp, sp, -16
+    sw ra, 12(sp)
+    sw a1, 4(sp)
 affine_fin_prologue:
     mv a1, a0
     mv a0, a2
     jal mult
-    lw a1, 1*4(sp)
+    lw a1, 4(sp)
     add a0, a0, a1
 affine_debut_epilogue:
-    lw ra, 3*4(sp)
-    addi sp, sp, 4*4
+    lw ra, 12(sp)
+    addi sp, sp, 16
     ret

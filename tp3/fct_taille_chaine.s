0aa4a87d2e26f447c1c309245a6af69988f9f63a
Mamadou <mamadou.thiongane@grenoble-inp.org>
2023-03-31 15:17:42 +0200

    taille_chaine

--- tp3/fct_taille_chaine.s
+++ tp3/fct_taille_chaine.s
@@ -13,20 +13,22 @@ uint32_t taille_chaine(const char *chaine)
 /* uint32_t taille_chaine(const char *chaine) */
 /* DEBUT DU CONTEXTE
 Fonction :
-    nom_de_fonction : feuille ou non feuille
+    taille_chaine : non feuille
 Contexte :
-    parametre_0        : registre a0
-    parametre_1        : registre ai; pile *(sp+n)        # 2 possibilités
-    parametre_64_bits  : registres t0 / t1                # 2 registres necessaires
-    variable_locale0   : registre t0
-    variable_locale1   : pile *(sp+k)
-    ra                 : pile *(sp+p)                     # cas particulier d'un registre à sauver
-    variable_64_bits   : pile *(sp+k1) / *(sp+k2)         # 2 registres necessaires
-    variable_globale0  : memoire
-    variable_globale1  : memoire, section nom_de_section  # ex de section : .data, .bss, .text, .rodata
+    chaine  : registre a0
+    taille  : registre t0
 FIN DU CONTEXTE */
 
 taille_chaine:
 taille_chaine_fin_prologue:
+    li t0, 0
+while:
+    lbu t1, 0(a0)
+    beqz t1, endwhile
+    addi t0, t0, 1
+    addi a0, a0, 1
+    j while
+endwhile:
+    mv a0, t0
 taille_chaine_debut_epilogue:
     ret

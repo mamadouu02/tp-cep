ad295925294032adfa3a1aa6e350855d078d8ef6
Mamadou <mamadou.thiongane@grenoble-inp.org>
2023-04-01 09:07:04 +0200

    fct_palin

--- tp3/fct_palin.s
+++ tp3/fct_palin.s
@@ -16,19 +16,41 @@ bool palin(const char *ch)
     /* bool palin(char *ch) */
 /* DEBUT DU CONTEXTE
 Fonction :
-    nom_de_fonction : feuille ou non feuille
+    palin : non feuille
 Contexte :
-    parametre_0        : registre a0
-    parametre_1        : registre ai; pile *(sp+n)        # 2 possibilités
-    parametre_64_bits  : registres t0 / t1                # 2 registres necessaires
-    variable_locale0   : registre t0
-    variable_locale1   : pile *(sp+k)
-    ra                 : pile *(sp+p)                     # cas particulier d'un registre à sauver
-    variable_64_bits   : pile *(sp+k1) / *(sp+k2)         # 2 registres necessaires
-    variable_globale0  : memoire
-    variable_globale1  : memoire, section nom_de_section  # ex de section : .data, .bss, .text, .rodata
+    ch  : registre a0; pile *(sp+0)
+    inf  : pile *(sp+4)
+    sup  : pile *(sp+8)
+    ra  : pile *(sp+12)
 FIN DU CONTEXTE */
 palin:
+    addi sp, sp, -16
+    sw ra, 12(sp)
 palin_fin_prologue:
+    sw zero, 4(sp)
+    sw a0, 0(sp)
+    jal strlen
+    addi t0, a0, -1
+    sw t0, 8(sp)
+    lw a0, 0(sp)
+while:
+    lw t0, 4(sp)
+    lw t1, 8(sp)
+    bge t0, t1, endwhile
+    add t2, a0, t0
+    lbu t2, 0(t2)
+    add t3, a0, t1
+    lbu t3, 0(t3)
+    bne t2, t3, endwhile
+    addi t0, t0, 1
+    addi t1, t1, -1
+    sw t0, 4(sp)
+    sw t1, 8(sp)
+    j while
+endwhile:
+    sltu a0, t0, t1
+    not a0, a0
 palin_debut_epilogue:
+    lw ra, 12(sp)
+    addi sp, sp, 16
     ret

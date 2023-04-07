31c605d20d40fe310549c2904464073cc710a74c
Mamadou <mamadou.thiongane@grenoble-inp.org>
2023-04-07 16:15:13 +0200

    abr_est_present

--- tp4/fct_abr_est_present.s
+++ tp4/fct_abr_est_present.s
@@ -19,19 +19,23 @@ bool abr_est_present(uint32_t val, struct noeud_t *abr)
 Fonction :
     abr_est_present : non feuille
 Contexte :
-    val  : registre a0
-    abr : registre a1
-    ra : pile *(sp+0)
+    val  : registre a0; pile *(sp+0)
+    abr : registre a1; pile *(sp+4)
+    ra : pile *(sp+8)
 FIN DU CONTEXTE */
 abr_est_present:
-    addi sp, sp, -4
-    sw ra, 0(sp)
+    addi sp, sp, -12
+    sw a0, 0(sp)
+    sw a1, 4(sp)
+    sw ra, 8(sp)
 abr_est_present_fin_prologue:
 if:
+    lw a1, 4(sp)
     bnez a1, elsif1
     li a0, 0
     j endif
 elsif1:
+    lw a0, 0(sp)
     lw t0, 0(a1)
     bne a0, t0, elsif2
     li a0, 1
@@ -46,6 +50,6 @@ else:
     jal abr_est_present
 endif:
 abr_est_present_debut_epilogue:
-    lw ra, 0(sp)
-    addi sp, sp, 4
+    lw ra, 8(sp)
+    addi sp, sp, 12
     ret

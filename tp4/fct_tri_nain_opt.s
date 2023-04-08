d55c714349809290402afe5e0b034b0232f3ddc3
Mamadou <mamadou.thiongane@grenoble-inp.org>
2023-04-08 20:46:59 +0200

    tri_nain_opt

--- tp4/fct_tri_nain_opt.s
+++ tp4/fct_tri_nain_opt.s
@@ -32,6 +32,8 @@ Contexte :
     i  : registre t0
     tmp : registre t1
     &tab[i] : registre t3
+    tab[i] : registre t1
+    tab[i+1] : registre t4
 FIN DU CONTEXTE */
 tri_nain_opt:
 tri_nain_opt_fin_prologue:

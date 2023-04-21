aaf9de4308c0db1dabbe2c26996832bf8842519f
Mamadou <mamadou.thiongane@grenoble-inp.org>
2023-04-21 16:48:08 +0200

    exo4.c

--- tp5/exo4.c
+++ tp5/exo4.c
@@ -1,10 +1,40 @@
-/* Ce fichier est intentionnellement vide.
-   Il sera à remplacer par le fichier de même nom accessible pendant l'examen.
-*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <inttypes.h>
 
+int32_t tab[100];
+
+extern uint32_t exo4(int32_t tab[], uint32_t taille, int32_t elem);
+
+uint32_t exo4_c(int32_t tab[], uint32_t taille, int32_t elem)
+{
+    uint32_t count = 0;
+    for (uint32_t i = 0; i < taille; i++) {
+        if (tab[i] != elem) {
+            count++;
+        }
+    }
+    return count;
+}
+
+static void init_tab(int32_t tab[], uint32_t taille)
+{
+    printf("Exemple de tableau : [");
+    for (uint32_t i = 0; i < taille - 1; i++) {
+        tab[i] = (random() % 19) - 9;
+		printf("%" PRId32 ", ", tab[i]);
+    }
+    printf("%" PRId32 "]\n", tab[taille - 1]);
+}
+
+
 int main(void) {
+    uint32_t taille = 17;
+    int32_t elem = -8;
+    srandom(0xbaffe);
+    init_tab(tab, taille);
+    uint32_t res_c = exo4_c(tab, taille, elem);
+    uint32_t res_as = exo4(tab, taille, elem);
+    printf("exo4 avec elem = %" PRId32 " calculé en C : %" PRIu32 " et en assembleur : %" PRIu32 "\n", elem, res_c, res_as);
     return 0;
 }

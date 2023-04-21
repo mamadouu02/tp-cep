0cc5429531934f0cb35c41be061e0531245ddd36
Mamadou <mamadou.thiongane@grenoble-inp.org>
2023-04-21 16:53:47 +0200

    fct_exo3

--- tp5/fct_exo3.s
+++ tp5/fct_exo3.s
@@ -1,8 +1,25 @@
-/* Ce fichier est intentionnellement vide.
-   Il sera à remplacer par le fichier homonyme accessible pendant l'examen.
+/*
+int exo3(int32_t x) {
+  int32_t val_fx = ma_fonction(x, 2*x);
+  int32_t val_fy = ma_fonction(2*x, x);
+    if (val_fy > val_fx) {
+        return 2*x;
+    } else {
+        return x;
+    }
+}
 */
     .text
     .globl exo3
+/* DEBUT DU CONTEXTE
+Fonction :
+    exo3 : non feuille
+Contexte :
+    x : registre a0; pile *(sp+0)
+    val_fx : registre t1; pile *(sp+4)
+    val_fy : registre t2; pile *(sp+8)
+    ra : pile *(sp+12)
+FIN DU CONTEXTE */
 exo3:
 exo3_fin_prologue:
 exo3_debut_epilogue:
